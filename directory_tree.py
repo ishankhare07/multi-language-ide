9b6ae92906d5bc7975dae9d45d9718fd7d20f964
Ishan Khare <ishankhare07@gmail.com>
2015-04-25 00:05:31 +0530

    changed contructor for providing directory name, renamed dir_tree in parse_tree method

--- directory_tree.py
+++ directory_tree.py
@@ -2,12 +2,13 @@ from gi.repository import Gtk
 import dir_core
 
 class Tree:
-    def __init__(self):
-        self.dir_tree = dir_core.Directory().childs
+    def __init__(self, directory=None):
+        self.dir_tree = dir_core.Directory(directory).childs
         self.treestore = Gtk.TreeStore(str)
+        self.parse_tree(self.dir_tree)
 
-    def parse_tree(self, dict_tree, parent=None):
-        for key, value in dict_tree.items():
+    def parse_tree(self, dir_tree, parent=None):
+        for key, value in dir_tree.items():
             if isinstance(value, dict):                         #is a directory
                 parent = self.treestore.append(None,[key])      #append directory to tree and store back treeiter
                 self.parse_tree(value, parent)                  #hence recurese into that directory
