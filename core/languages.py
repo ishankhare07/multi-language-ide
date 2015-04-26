b7312e2c575c0928a9a9270b83ece749d7f3dace
Ishan Khare <ishankhare07@gmail.com>
2015-04-27 01:28:03 +0530

    major reorganization of files separated files into core and gui directories gui subdivided into a bind directory for now -> contains dynamically attached widget files a new main gui file new main python file

---
+++ core/languages.py
@@ -0,0 +1,17 @@
+compilers = {
+        'c' : ['gcc','.c'],
+        'cpp' : ['g++','.cpp'],
+        'java' : ['javac','.java']
+}
+
+interpreters = {
+        'python' : ['python','.py'],
+        'python3' : ['python3', '.py'],
+        'ruby' : ['ruby','.rb'],
+        'perl' : ['perl','.pl']
+}
+
+
+class Language:
+	def change_lanugage(self,name):
+		pass
---
+++ main.py
@@ -0,0 +1,21 @@
+from gi.repository import Gtk
+
+class Main:
+    def __init__(self, builder):
+        """
+            something goes here
+        """
+
+        self.builder = builder
+
+    def on_destroy(self, *args):
+        Gtk.main_quit(*args)
+
+
+builder = Gtk.Builder()
+builder.add_from_file('gui/main.glade')
+builder.connect_signals(Main(builder))
+window = builder.get_object('window1')
+
+window.show_all()
+Gtk.main()
