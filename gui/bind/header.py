ae4d309eb1ce99683995d8e9e9b55c7e631243ba
Ishan Khare <ishankhare07@gmail.com>
2015-05-04 01:40:33 +0530

    organized the header file

--- gui/bind/header.py
+++ gui/bind/header.py
@@ -4,18 +4,14 @@ from gi.repository import Gtk, GtkSource
 
 class Header:
     def __init__(self):
+        """
+            creates the combobox to be used in header
+        """
         self.liststore = Gtk.ListStore(str)
         for lang in GtkSource.LanguageManager().get_language_ids():
             self.liststore.append([lang])
-        self.widget = Gtk.ComboBox(model=self.liststore)
+        self.combobox = Gtk.ComboBox()
+        self.combobox.set_model(self.liststore)
         crt = Gtk.CellRendererText()
-        self.widget.pack_start(crt, True)
-        self.widget.add_attribute(crt, "text", 0)
-
-        self.widget.connect('changed', self.language_changed)
-
-    def language_changed(self, widget):
-        index = widget.get_active()
-        model = widget.get_model()
-        lang = model[index][0]
-        print(lang)
+        self.combobox.pack_start(crt, True)
+        self.combobox.add_attribute(crt, "text", 0)
