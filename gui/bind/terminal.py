56b002c1fce093b2b779c708515f44a73306a700
Ishan Khare <ishankhare07@gmail.com>
2015-05-12 12:11:27 +0530

    moved from vte2.90 to latest vte 2.91 new api renames: Vte.Terminal().fork_command_full -> Vte.Terminal().spawn_sync

--- gui/bind/terminal.py
+++ gui/bind/terminal.py
@@ -1,22 +1,23 @@
 __author__ = 'ishan'
 
-import gi
-gi.require_version("Vte", "2.90")
 from gi.repository import Gtk, Vte, GLib, Gdk
 import os
 
 class Terminal(Vte.Terminal):
-    def __init__(self):
+    def __init__(self, cwd=''):
         Vte.Terminal.__init__(self)
-        self.fork_command_full(
-            Vte.PtyFlags.DEFAULT,
-            os.environ['HOME'],
-            ['/bin/sh'],
-            [],
-            GLib.SpawnFlags.DO_NOT_REAP_CHILD,
-            None,
-            None
+        self.spawn_sync(
+            Vte.PtyFlags.DEFAULT,               # flags from Vte.PtyFlags
+            cwd,                                # set current working directory
+            ['/bin/sh'],                        # child’s argument vector
+            [],                                 # a list of environment variables to be added to the environment
+                                                # before starting the process, or None
+            GLib.SpawnFlags.DO_NOT_REAP_CHILD,  # flags from GLib.SpawnFlags
+            None,                               # an extra child setup function to run in the child just before exec(),
+                                                # or None
+            None,                               # user data for child_setup
+            None                                # a Gio.Cancellable, or None
         )
 
         # set a green font foreground color
-        self.set_color_foreground(Gdk.RGBA(0, 1, 0, 1).to_color())
+        self.set_color_foreground(Gdk.RGBA(0, 1, 0, 1))
